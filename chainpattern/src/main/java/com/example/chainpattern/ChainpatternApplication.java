23179d1c5b9143ef4bb9aa3be0664ce588a914fb
liudengfeng <liudengfeng@taoche.com>
2021-10-22 15:58:29 +0800

    更新代码

--- chainpattern/src/main/java/com/example/chainpattern/ChainpatternApplication.java
+++ chainpattern/src/main/java/com/example/chainpattern/ChainpatternApplication.java
@@ -1,14 +1,15 @@
 package com.example.chainpattern;
 
-import com.example.chainpattern.service.dnsfirst.*;
+import com.alibaba.fastjson.JSON;
+import com.example.chainpattern.service.*;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
 
 @SpringBootApplication
 public class ChainpatternApplication implements CommandLineRunner {
@@ -22,22 +23,23 @@ public class ChainpatternApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//		System.out.println("模拟开始...");
-//		Random random = new Random();
-//		ArrayList<IWomen> arrayList = new ArrayList<>();
-//		for (int i = 0; i < 5; i++) {
-//			IWomen women = new Women(random.nextInt(3) + 1, "我要去逛街");
-//			arrayList.add(women);
-//			System.out.println(String.format("打印第%s条记录信息:%s", i, JSON.toJSONString(women)));
-//		}
-//		Handler father = new Father();
-//		Handler husband = new Husband();
-//		Handler son = new Son();
-//		father.setNextHandler(husband);
-//		husband.setNextHandler(son);
-//		for (IWomen iWomen : arrayList) {
-//			father.HandleMessage(iWomen);
-//		}
+		System.out.println("模拟开始...");
+		Random random = new Random();
+		ArrayList<IWomen> arrayList = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			IWomen women = new Women(random.nextInt(3) + 1, "我要去逛街");
+			arrayList.add(women);
+			System.out.println(String.format("打印第%s条记录信息:%s", i, JSON.toJSONString(women)));
+		}
+
+		Handler father = new Father();
+		Handler husband = new Husband();
+		Handler son = new Son();
+		father.setNextHandler(husband);
+		husband.setNextHandler(son);
+		for (IWomen iWomen : arrayList) {
+			father.HandleMessage(iWomen);
+		}
 
 //        System.out.println("模拟开始....");
 //        DnsParse dnsOfShanghai = new DnsOfShanghai();
@@ -49,22 +51,22 @@ public class ChainpatternApplication implements CommandLineRunner {
 //        requestDns.printRequestMsg();
 //        dnsOfShanghai.parse(requestDns);
 
-        DnsServer sh = new SHDnsServer();
-        DnsServer china = new ChinaTopDnsServer();
-        DnsServer top = new TopDnsServer();
-        sh.setUpperServer(china);
-        china.setUpperServer(top);
-        System.out.println("===域名解析模拟器===");
-        while (true){
-            System.out.println("\n请输入域名(输入N退出):");
-            String domain = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-            if(domain.equalsIgnoreCase("n")){
-                return;
-            }
-            Recorder recorder = sh.resolve(domain);
-            System.out.println("---DNS服务器解析结果---");
-            System.out.println(recorder);
-        }
+//        DnsServer sh = new SHDnsServer();
+//        DnsServer china = new ChinaTopDnsServer();
+//        DnsServer top = new TopDnsServer();
+//        sh.setUpperServer(china);
+//        china.setUpperServer(top);
+//        System.out.println("===域名解析模拟器===");
+//        while (true){
+//            System.out.println("\n请输入域名(输入N退出):");
+//            String domain = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+//            if(domain.equalsIgnoreCase("n")){
+//                return;
+//            }
+//            Recorder recorder = sh.resolve(domain);
+//            System.out.println("---DNS服务器解析结果---");
+//            System.out.println(recorder);
+//        }
 
 //        DnsServer sh = new SHDnsServer();
 //        DnsServer china = new ChinaTopDnsServer();
