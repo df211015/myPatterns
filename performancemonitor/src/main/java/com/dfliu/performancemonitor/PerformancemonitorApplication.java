abff05acf51b104725c55455b73aa14d7fe59b7b
liudengfeng <liudengfeng@taoche.com>
2021-11-24 17:38:27 +0800

    性能计数器项目提交

---
+++ performancemonitor/src/main/java/com/dfliu/performancemonitor/PerformancemonitorApplication.java
@@ -0,0 +1,13 @@
+package com.dfliu.performancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformancemonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformancemonitorApplication.class, args);
+    }
+
+}
---
+++ performancemonitor/src/test/java/com/dfliu/performancemonitor/PerformancemonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfliu.performancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformancemonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
