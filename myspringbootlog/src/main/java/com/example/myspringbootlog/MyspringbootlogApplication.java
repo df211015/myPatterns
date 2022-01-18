6eb7ce5c2280d5eb8e7a559f1d0a796172f85e1b
liudengfeng <liudengfeng@taoche.com>
2022-01-18 15:07:30 +0800

    追加springboot-log的模式设计模式解析项目

--- myspringbootlog/src/main/java/com/example/myspringbootlog/MyspringbootlogApplication.java
+++ myspringbootlog/src/main/java/com/example/myspringbootlog/MyspringbootlogApplication.java
@@ -1,13 +1,23 @@
 package com.example.myspringbootlog;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyspringbootlogApplication {
+@Slf4j
+public class MyspringbootlogApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MyspringbootlogApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("程序启动成功!");
+        log.info("log.info");
+        log.debug("log.debug");
+        log.error("log.error");
+    }
 }
