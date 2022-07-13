47acbe4a934fde613cbe8dc8d032ecaec319f33a
liudengfeng <liudengfeng@taoche.com>
2022-07-13 17:41:41 +0800

    更新日期时间

--- demo100/src/main/java/com/example/demo100/Demo100Application.java
+++ demo100/src/main/java/com/example/demo100/Demo100Application.java
@@ -5,11 +5,15 @@ import cn.yichehuoban.op.usedcarapi.dto.DealerAccountVo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 @SpringBootApplication
 public class Demo100Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo100Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Demo100Application.class, args);
 
 //		Request req = new Request();
 //		req.setRequestStr("<script>,hello,敏感词汇！");
@@ -42,10 +46,22 @@ public class Demo100Application {
 //		BeanUtils.copyProperties(carBasicVo, simpleCarBasicVo);
 //		BeanUtils.copyProperties(carBasicVo.getInnercls001(),simpleCarBasicVo.getInnercls001());
 
-		//{"token":"5a390bb60e0a4699812c1e568d14d443","env":"test","sysCode":"app","authorization":"5a390bb60e0a4699812c1e568d14d443","accountId":null,"dealerId":null,"tcDealerId":null}
-		DealerAccountVo userInfoByToten = PermissionUtils.getUserInfoByToten("test", "5a390bb60e0a4699812c1e568d14d443", "app");
+        //{"token":"5a390bb60e0a4699812c1e568d14d443","env":"test","sysCode":"app","authorization":"5a390bb60e0a4699812c1e568d14d443","accountId":null,"dealerId":null,"tcDealerId":null}
+//		DealerAccountVo userInfoByToten = PermissionUtils.getUserInfoByToten("test", "5a390bb60e0a4699812c1e568d14d443", "app");
 
-		String str = "";
-	}
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new java.util.Date());
+        calendar.add(Calendar.DATE, -21);
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+        Date time = calendar.getTime();
+
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), 20, 0, 0, 0);
+        Date time2 = calendar.getTime();
+
+        boolean after = time.after(time2);
+        boolean before = time.before(time2);
+
+        String str = "";
+    }
 
 }
